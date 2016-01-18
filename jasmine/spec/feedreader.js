7b6c5fe3846e276d9fd6213ac8de67b687cec093
Turtle-Head <djinn.jecture@gmail.com>
2016-01-18 00:10:03 -0600

    removed unused variable

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -54,8 +54,7 @@ $(function() {
 
     /* TODO: Write a new test suite named "The menu" */
     describe('The Menu', function(){
-      var spyEvent;
-
+      
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
